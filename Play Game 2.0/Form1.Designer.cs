0a65a8f3799ceb232afd18b0ee2d2da54f9ddbde
aubrcart224 <aubrcart224@ed.amdsb.ca>
2021-04-23 09:34:21 -0400

    Add project files.

---
+++ Play Game 2.0/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Play_Game_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.Black;
+            this.button.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button.ForeColor = System.Drawing.Color.White;
+            this.button.Location = new System.Drawing.Point(293, 161);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(180, 80);
+            this.button.TabIndex = 0;
+            this.button.Text = "Click To Play";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(205, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 80);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+        
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Play Game ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Play Game 2.0/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace Play_Game_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible = true;
+            
+        }
+
+        
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            //sound 
+
+            //SoundPlayer bleepPlayer = new SoundPlayer(A_Tone_His_Self_1266414414__1_);
+
+            // button vanish countdown begin 
+
+            button.Visible = false;
+            //this.label1.ForeColor = Color.White;
+            Refresh();
+            Thread.Sleep(1100);
+            label1.Visible = true;
+            //bleepPlayer.Play();
+            label1.Text = "Starting in 3";
+            Refresh();
+            Thread.Sleep(1100);
+            label1.Text = "Starting in 2";
+            Refresh();
+            Thread.Sleep(1100);
+            label1.Text = "Starting in 1";
+            Refresh();
+            Thread.Sleep(1100);
+            label1.Text = " GO ";
+            
+
+            //go green 
+            this.BackColor = Color.Green;
+        }
+
+        
+    }
+}
